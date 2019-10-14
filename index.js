4f5f651e23dbef59977e6b67dcb0fe370b107706
Jamie Luong <jamieluong03@gmail.com>
2019-10-14 15:16:06 -0700

    no more errors! except for electron

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const fs = require("fs"),
 convertFactory = require('electron-html-to');
  
-var conversion = convertFactory({
-    converterPath: convertFactory.converters.PDF
-  });
 const axios = require("axios");
 const inquirer = require("inquirer");
 var {generateHTML} = require("./generateHTML.js");
 
 
-console.log(generateHTML);
+// console.log(generateHTML);
 // generateHTML()
 
 function promptUser(){
@@ -27,78 +24,51 @@ function promptUser(){
     }]
 )};
 
-
-function init(data) {
-    let user = data.name;
-    let location = data.location;
-    let gitProf = data.html_url;
-    let userBlog = data.blog;
-    let userBio = data.bio;
-    let pubRepo = data.public_repos;
-    let userFollowers = data.followers;
-    let userFollowing = data.following;
-    // put info in an object
-    let data = {
-        user,
-        location,
-        gitProf, 
-        userBlog,
-        userBio,
-        pubRepo,
-        userFollowers,
-        userFollowing
-    };
-
-    // return the object 
-    return data;
-}
-
-
 promptUser()
     .then(function({username, color}){
         const queryURL = `https://api.github.com/users/${username}`;
-        const starURL = `https://api.github.com/users/${username}/repos?per_page=100`;
+        // const starURL = `https://api.github.com/users/${username}/repos?per_page=100`;
 
         // let userInfo = await getUserInfo(queryURL);
         // let stars = await getStars(starURL);
 
     axios.get(queryURL)
     .then(({data}) => {
-        console.log(data);
-        axios.get(starURL)
-        .then(({stars}) => {
-            // pass stars through a function herefunction getTotalStars(stars){
-            for (i=0; i<stars.length-1; i++){
-                let starred = stars.data[i].stargazers_count;
-                starred += starred;
-                console.log(starred);
-            };
-            return starred;
-        });
+        // console.log(data);
 
-            init(data);
-            
+        var conversion = convertFactory({
+            converterPath: convertFactory.converters.PDF
+          });
+
+          let html = generateHTML({color, data})
+
+          conversion({ html }, function(err, result) {
+            if (err) {
+              return console.error(err);
+            }
+           
+            result.stream.pipe(fs.createWriteStream('profile.pdf'));
+            conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+          });
+        // axios.get(starURL)
+        // .then(({stars}) => {
+        //     // pass stars through a function herefunction getTotalStars(stars){
+        //     for (i=0; i<stars.length-1; i++){
+        //         let starred = stars.data[i].stargazers_count;
+        //         starred += starred;
+        //         console.log(starred);
+        //     };
+        //     return starred;
+        // });            
             // let totalStarss = return value of function (getTotalValueOfStars(stars))
             // console.log(stars)
-            return generateHTML({starred, color, data})
         
-        .catch(function(err){
-            console.log(err);
-        });
+    //     .catch(function(err){
+    //         console.log(err);
+    //     });
     })
-    .catch(function(err){
-        console.log(err);
-    })
+    // .catch(function(err){
+    //     console.log(err);
+    // })
 
-});
-
-
- 
-conversion({ html: generateHTML }, function(err, result) {
-  if (err) {
-    return console.error(err);
-  }
- 
-  result.stream.pipe(fs.createWriteStream('profile.pdf'));
-  conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
 });
